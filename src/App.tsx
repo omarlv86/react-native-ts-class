210ece83109dabfe5a7e42d16537963a89f2784b
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-08-25 18:00:11 -0500

    Main | AuthReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { ContadorConHook } from './components/ContadorConHook'
+import { Login } from './components/Login';
+/* import { ContadorConHook } from './components/ContadorConHook' */
 /* import { Contador } from './components/Contador' */
 /* import { Funciones } from './typescript/Funciones' */
 //import { ObjetosLiterales } from './typescript/ObjetosLiterales'
@@ -13,7 +14,8 @@ const App = () => {
       {/*<ObjetosLiterales/>*/}
       {/* <Funciones/> */}
       {/* <Contador/> */}
-      <ContadorConHook/>
+      {/* <ContadorConHook/> */}
+      <Login/>
     </div>
   )
 }
