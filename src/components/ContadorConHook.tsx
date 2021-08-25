0bb72d5ca61ae4ffcfab492c3f19cd84e9885614
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-08-25 17:10:58 -0500

    Main | Custom Hook - UseCounter

---
+++ src/components/ContadorConHook.tsx
@@ -0,0 +1,19 @@
+/* import { useState } from "react" */
+import { useCounter } from "../hooks/useCounter"
+
+export const ContadorConHook = () => {
+
+    const { valor, acumular} = useCounter(4);
+    return (
+        <>
+            <h3>Contador con Hook: <small>{valor}</small></h3>
+            <button className="btn btn-primary" onClick={() => acumular(1)}>
+                +1
+            </button>
+            &nbsp;
+            <button className="btn btn-secondary" onClick={() => acumular(-1)}>
+                -1
+            </button>
+        </>
+    )
+}
