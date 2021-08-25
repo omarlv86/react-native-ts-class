168f145ed1803d0a772304d96019713955990566
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-08-25 16:10:07 -0500

    Main | Tipos basicos

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,8 +1,19 @@
 
 export const TiposBasicos = () => {
+    const nombre: string = 'Omar';
+    const edad: number = 35;
+    const estaActivo:boolean = true;
+    const poderes:string[] = ['Velocidad', 'Volar','Respirar en el agua'];
+
+    //poderes.push(1);
+    poderes.push('123');
+    //poderes.push(true);
     return (
         <div>
             <h3>Tipos Basicos</h3>
+            { nombre }, { edad }, {(estaActivo) ?'activo':'no activo'}
+            <br/>
+            {poderes.join(', ')}
         </div>
     )
 }
